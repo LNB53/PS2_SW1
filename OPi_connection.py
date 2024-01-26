d8b699d4e95d881088f3c578e1139c42cb61f9c2
wobr53 <r0898448@student.thomasmore.be>
2024-01-26 09:43:00 +0100

    Opi connect to API

--- OPi_connection.py
+++ OPi_connection.py
@@ -2,6 +2,7 @@ import wiringpi
 import time
 import sys
 import threading
+import requests
 
 # Functions
 def lights_off():
@@ -9,8 +10,8 @@ def lights_off():
     wiringpi.digitalWrite(LED_2, 0)
     wiringpi.digitalWrite(LED_3, 0)
     wiringpi.digitalWrite(LED_4, 0)
-    wiringpi.digitalWrite(SEND_LED, 0)
-    wiringpi.digitalWrite(TRUE_LED, 0)
+    wiringpi.digitalWrite(RED_LED, 0)
+    wiringpi.digitalWrite(GREEN_LED, 0)
 
 def blink(_pin):
     wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
@@ -19,19 +20,19 @@ def blink(_pin):
     time.sleep(0.1)
 
 # Input pins
-BUTTON_1 = 0
-BUTTON_2 = 1
-BUTTON_3 = 2
-BUTTON_4 = 3
-SEND_BUTTON = 8
+BUTTON_1 = 0            # Yellow, taped
+BUTTON_2 = 1            # Green, taped
+BUTTON_3 = 2            # Purple, taped
+BUTTON_4 = 3            # Gray, taped
+SEND_BUTTON = 8         # Brown, taped
 
 # Output pins
-LED_1 = 4
-LED_2 = 5
-LED_3 = 6
-LED_4 = 7
-SEND_LED = 9
-TRUE_LED = 10
+LED_1 = 4               # Yellow
+LED_2 = 5               # Green
+LED_3 = 6               # Purple
+LED_4 = 7               # Gray
+RED_LED = 9            # Black
+GREEN_LED = 10           # Orange
 
 # General setup
 wiringpi.wiringPiSetup()
@@ -44,10 +45,16 @@ wiringpi.pinMode(LED_1, 1)
 wiringpi.pinMode(LED_2, 1)
 wiringpi.pinMode(LED_3, 1)
 wiringpi.pinMode(LED_4, 1)
-wiringpi.pinMode(SEND_LED, 1)
-wiringpi.pinMode(TRUE_LED, 1)
+wiringpi.pinMode(RED_LED, 1)
+wiringpi.pinMode(GREEN_LED, 1)
 lights_off()
 print("Start")
+
+# Var
+
+student_id = "r0898448"
+answer = 0
+hosting_url = "https://rotten-ducks-sip.loca.lt/"
 
 # Main
 
@@ -62,42 +69,56 @@ try:
         if A == 0:
             lights_off()
             print("A")
+            answer = "A"
             wiringpi.digitalWrite(LED_1, 1)
             time.sleep(2)
 
         if B == 0:
             lights_off()
             print("B")
+            answer = "B"
             wiringpi.digitalWrite(LED_2, 1)
             time.sleep(2)
 
         if C == 0:
             lights_off()
             print("C")
+            answer = "C"
             wiringpi.digitalWrite(LED_3, 1)
             time.sleep(2)
 
         if D == 0:
             lights_off()
             print("D")
+            answer = "D"
             wiringpi.digitalWrite(LED_4, 1)
             time.sleep(2)
 
-        if (wiringpi.digitalRead(SEND_BUTTON) == 0 and 
-            (A == 0 or B == 0 or C == 0 or D == 0)):
+        if wiringpi.digitalRead(SEND_BUTTON) == 0 and answer != 0:
             lights_off()
             on = False
             print("\nSending Answer")
-            i = 1
-            while i <= 20:
-                blink(SEND_LED)
-                i += 1
-            wiringpi.digitalWrite(SEND_LED, 1)
-            time.sleep(3)
-            wiringpi.digitalWrite(SEND_LED, 0)
-            time.sleep(1)
-            wiringpi.digitalWrite(TRUE_LED, 1)
-            time.sleep(3)
+
+            api_url = f"{hosting_url}submit_answer/{student_id}/{answer}"
+            print(api_url)
+
+            answer_data = {
+                    "student_id": student_id,
+                    "answer": answer
+                }
+            
+            requests.post(api_url, json=answer_data)
+
+            # i = 1
+            # while i <= 20:
+            #     blink(RED_LED)
+            #     i += 1
+            # wiringpi.digitalWrite(RED_LED, 1)
+            # time.sleep(3)
+            # wiringpi.digitalWrite(RED_LED, 0)
+            # time.sleep(1)
+            # wiringpi.digitalWrite(GREEN_LED, 1)
+            # time.sleep(3)
 
     lights_off()
     print("\nEnd")
